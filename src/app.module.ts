30ffb488fc51c473e04454189064790e67e17139
Talhas07 <137061576+Talhas07@users.noreply.github.com>
2023-08-21 16:33:37 +0500

    all modules plus atlas config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { StreamModule } from './stream/stream.module';
+import { EpisodeModule } from './episode/episode.module';
+import { SeasonModule } from './season/season.module';
+import { SeriesModule } from './series/series.module';
+
+import { FileModule } from './file/file.module';
+import { GenreSeriesModule } from './genre-series/genre-series.module';
+import { GenreModule } from './genre/genre.module';
 @Module({
   imports: [
     UserModule,
+    StreamModule,
+    EpisodeModule,
+    SeasonModule,
+    SeriesModule,
+    GenreSeriesModule,
+    GenreModule,
+    FileModule,
+
     MongooseModule.forRoot(
       'mongodb+srv://mtalhasaleem22:NoMyWl1GdLrzeJ9j@database.1u29mq1.mongodb.net/?retryWrites=true&w=majority',
     ),

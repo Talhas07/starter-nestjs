eb2e06c9b97a277f9e0775acf7307bd850b13d1b
Talhas07 <137061576+Talhas07@users.noreply.github.com>
2023-08-21 15:37:09 +0500

    users added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://mtalhasaleem22:NoMyWl1GdLrzeJ9j@database.1u29mq1.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
